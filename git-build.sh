fbfb932e6f9a15cb4547bb99f1ace4cd0822f538
Rosh PR <roshpr@gmail.com>
2021-03-12 13:44:20 -0800

    Cd to docker dir

--- git-build.sh
+++ git-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd docker
 docker build --progress=plain -t git-server:latest .
 
 #docker tag git-server:latest svl-artifactory.juniper.net/atom-docker/atom-of/jdeployer/git-server:latest
