607e7d6e83b494043294040e0799c609d51c2d7d
roshpr <roshpr@juniper.net>
2021-03-12 13:43:04 -0800

    Initial Helm and docker configs

---
+++ git-build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build --progress=plain -t git-server:latest .
+
+#docker tag git-server:latest svl-artifactory.juniper.net/atom-docker/atom-of/jdeployer/git-server:latest
+#docker push svl-artifactory.juniper.net/atom-docker/atom-of/jdeployer/git-server:latest
